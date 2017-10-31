6260e5abb04dad60c5993f8d83817fb4d0d9774f
Apurva Pandey <apurva.pandey@incture.com>
2017-10-30 20:35:40 -0400

    First Commit on Github

---
+++ public/javascripts/pbi.js
@@ -0,0 +1,133 @@
+
+//$('#embedContainer').width($('#embedContainer').width()-2)
+$('#embedContainer').height($('#embedContainer').width()*.475)
+
+
+
+var models = window['powerbi-client'].models;
+var permissions = models.Permissions.All;
+
+var intervalId;
+
+function createConfig(embedToken,reportId,groupId) {
+
+  var embedUrl = 'https://app.powerbi.com/reportEmbed?reportId='+reportId+'&groupId='+groupId;  
+  
+  var embedConfiguration = {
+    type: 'report',
+    id: reportId,
+    embedUrl: embedUrl,
+    tokenType: models.TokenType.Embed,
+    permissions:permissions,
+    settings: {
+          filterPaneEnabled: false,
+          navContentPaneEnabled: false
+      },
+    accessToken: embedToken
+  };
+
+  return embedConfiguration;
+
+}
+
+
+function embedReportAndSetTokenListener(reportId,groupId) {
+    // Generate embed token
+  //  generateEmbedToken(reportId, groupId)
+
+  var embedContainer = $('#embedContainer')[0];
+ 
+    $.get( "/token/group/"+groupId+"/report/"+reportId).then(function( Token ) {
+
+        var embedToken = JSON.parse(Token).token;
+        
+        // set config for embedding report
+        var config = createConfig(embedToken,reportId,groupId);
+        
+      //  console.log(config);
+        // Get a reference to the embedded report HTML element
+        
+        
+        // Embed the report and display it within the div container.
+        var report = powerbi.embed(embedContainer, config);
+        
+        // Report.off removes a given event handler if it exists.        
+        report.off("loaded");
+
+
+        // Report.on will add an event handler which prints to Log window.
+        report.on("loaded", function() {
+           // console.log("Clearning interval"+intervalId);
+            clearInterval(intervalId);
+            //console.log("Setting new interval");
+            intervalId=setInterval(function(){ report.refresh() }, 40000);  
+            
+            setTokenExpirationListener(JSON.parse(Token).expiration,
+            2 /*minutes before expiration*/, 
+            reportId, 
+            groupId);
+        });
+    });
+}
+
+function setTokenExpirationListener(tokenExpiration, 
+    minutesToRefresh = 2, 
+    reportId, 
+    groupId){
+    
+    // get current time
+    var currentTime = Date.now();
+    var expiration = Date.parse(tokenExpiration);
+    var safetyInterval = minutesToRefresh* 60 * 1000;
+
+    // time until token refresh in milliseconds
+    var timeout = expiration - currentTime - safetyInterval;
+    
+
+    // if token already expired, generate new token and set the access token
+    if (timeout<=0)
+    {
+        console.log("Updating Report Embed Token" + reportId);
+        updateToken(reportId, groupId);
+    }
+    // set timeout so minutesToRefresh minutes before token expires, token will be updated
+    else 
+    {
+        console.log("Report Embed Token will be updated in " + timeout + " milliseconds.");
+        setTimeout(function() {
+        updateToken(reportId, groupId);
+        }, timeout);
+    }
+}
+
+function updateToken(reportId, groupId) {
+    // Generate new EmbedToken
+    $.get( "/token/group/"+groupId+"/report/"+reportId)
+    .then(function( Token ) {
+        // Get a reference to the embedded report HTML element
+        var embedContainer = $('#embedContainer')[0];
+        // Get a reference to the embedded report.
+        var report = powerbi.get(embedContainer);
+
+        // Set AccessToken
+        report.setAccessToken(JSON.parse(Token).token)
+        .then(function() {
+        // Set token expiration listener
+        // result.expiration is in ISO format
+        setTokenExpirationListener(JSON.parse(Token).expiration,2,reportId,groupId /*minutes before expiration*/);
+        });
+    });
+}
+
+function embedReport(reportId,element){
+ $("li").removeClass("active");
+ $("#"+element).addClass("active");
+ embedReportAndSetTokenListener(reportId,gid) 
+}
+
+function fullscreen(){
+  var element = $('#embedContainer')[0];
+  var report = powerbi.get(element);
+
+  report.fullscreen();
+}
---
+++ routes/token.js
@@ -0,0 +1,72 @@
+var express = require('express');
+var request = require('request');
+var MultiKeyCache = require('multi-key-cache')
+
+var multiKeyCache = new MultiKeyCache()
+var router = express.Router()
+
+
+
+router.get('/group/:gid/report/:rid', function(req, res, next) {
+
+
+if(multiKeyCache.has(['embedToken',req.params.gid,req.params.rid])) {
+
+	var embedTokenResponse = multiKeyCache.get(['embedToken',req.params.gid,req.params.rid])
+	//console.log("Found cache"+embedTokenResponse)
+	var currentTime = Date.now();
+    var expiration = Date.parse(JSON.parse(embedTokenResponse).expiration);
+    var timeout = expiration - currentTime - 10*60*1000;
+
+ 	if (timeout>0) {
+ 		console.log("Sending reponse from cache")
+ 		res.send(embedTokenResponse);
+ 	}
+ 	else {
+ 		console.log("Cache exprired - Getting New token")
+ 		getNewToken(req.params.gid,req.params.rid,res)
+ 	}
+
+}
+else {
+	console.log("Cache not found - Getting New token")
+	getNewToken(req.params.gid,req.params.rid,res)
+ }
+  
+});
+
+function getNewToken(groupId,reportId,res) {
+	var requestBody = 'grant_type=password&client_id='+process.env.CLIENTID+'&username='+process.env.PBIUSERNAME+'&password='+process.env.PBIPASSWORD+'&resource=https://analysis.windows.net/powerbi/api'
+	request({
+	    url: "https://login.microsoftonline.com/freshdirect.onmicrosoft.com/oauth2/token",
+	    method: "POST",
+	    headers: {
+	        "content-type": "application/x-www-form-urlencoded",  // 
+	    },
+	    body: requestBody
+	}, function (error, response, body){
+
+	if(!error) {
+		var embedTokenbody = "{\"accessLevel\": \"View\"}"
+		request({
+		    url: "https://api.powerbi.com/v1.0/myorg/groups/"+groupId+"/reports/"+reportId+"/GenerateToken",
+		    method: "POST",
+		    headers: {
+		        "content-type": "application/json",
+		        "Authorization": "Bearer "+JSON.parse(response.body).access_token
+		    },
+		    body: embedTokenbody
+		}, function (error, response, body){
+				if(!error) {
+					multiKeyCache.set(['embedToken',groupId,reportId],response.body)
+					//multiKeyCache.delete(['embedToken',groupId,reportId])
+					//console.log("Sending reponse from new"+response.body)
+		    		res.send(response.body);
+		    	}
+			});
+	}
+    
+	});
+}
+
+module.exports = router;
