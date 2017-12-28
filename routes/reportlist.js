343d8976a4afb32f3cec7be2d55e6b0a23dddb18
Apurva Pandey <apurva.pandey@incture.com>
2017-12-27 22:14:39 -0500

    changed refresh freqs

--- routes/reportlist.js
+++ routes/reportlist.js
@@ -6,7 +6,7 @@ var reportList =
 		      rid: 'f837b929-4504-4709-a8ad-c05e05b281a7',
 		      gid: 'b1717488-1ca0-4f42-8da3-1fc7c1363833',
 		      name: 'Dispatch Monitor',
-		      refresh: 60,
+		      refresh: 90,
 		      navBar: false
 		    },
 		    throughput:{
@@ -14,7 +14,7 @@ var reportList =
 		      rid: '33e15624-6a78-4f4f-aedc-1814460bc578',
 		      gid: 'b1717488-1ca0-4f42-8da3-1fc7c1363833',
 		      name: 'Throughput',
-		      refresh: 60,
+		      refresh: 90,
 		      navBar: false
 		    },
 		    orderqueue:{
@@ -22,7 +22,7 @@ var reportList =
 		      rid: '3baf5742-4ccf-48cb-ba22-579733e938d2',
 		      gid: 'b1717488-1ca0-4f42-8da3-1fc7c1363833',
 		      name: 'Order Queue',
-		      refresh: 60,
+		      refresh: 90,
 		      navBar: false
 		    },
 		    mailslot:{
@@ -30,7 +30,7 @@ var reportList =
 		      rid: '91ee18cc-8301-4d93-b801-d0535332c3e9',
 		      gid: 'b1717488-1ca0-4f42-8da3-1fc7c1363833',
 		      name: 'Mail Slot',
-		      refresh: 60,
+		      refresh: 90,
 		      navBar: false
 		    },
 		    ambient:{
@@ -38,7 +38,7 @@ var reportList =
 		      rid: 'c7c4974b-eba8-4e8a-9fd1-3dc9bb47c7f3',
 		      gid: 'b1717488-1ca0-4f42-8da3-1fc7c1363833',
 		      name: 'Ambient',
-		      refresh: 60,
+		      refresh: 90,
 		      navBar: false
 		    },
 		    fresh:{
@@ -46,7 +46,7 @@ var reportList =
 		      rid: 'b462e136-dc9e-46ba-97ac-194a366cb04a',
 		      gid: 'b1717488-1ca0-4f42-8da3-1fc7c1363833',
 		      name: 'Fresh',
-		      refresh: 60,
+		      refresh: 90,
 		      navBar: false
 		    },
 		    chill:{
@@ -54,7 +54,7 @@ var reportList =
 		      rid: '86deea9b-138b-41dd-8f69-8c3d1f8543b6',
 		      gid: 'b1717488-1ca0-4f42-8da3-1fc7c1363833',
 		      name: 'Chill',
-		      refresh: 60,
+		      refresh: 90,
 		      navBar: false
 		    },
 		    qcmezz:{
@@ -62,7 +62,7 @@ var reportList =
 		      rid: '55880244-0603-4759-886b-43522a51c33d',
 		      gid: 'b1717488-1ca0-4f42-8da3-1fc7c1363833',
 		      name: 'QC Mezz',
-		      refresh: 60,
+		      refresh: 90,
 		      navBar: false
 		    },
 		    shipfloor:{
@@ -70,7 +70,7 @@ var reportList =
 		      rid: 'b2f21019-7dda-4b0e-aed1-16eba6b1b021',
 		      gid: 'b1717488-1ca0-4f42-8da3-1fc7c1363833',
 		      name: 'Ship Floor',
-		      refresh: 60,
+		      refresh: 90,
 		      navBar: false
 		    },
 		    dockdoor:{
@@ -78,7 +78,7 @@ var reportList =
 		      rid: '4c477360-5366-45a5-b968-feec1ed9a90e',
 		      gid: 'b1717488-1ca0-4f42-8da3-1fc7c1363833',
 		      name: 'Dock Door',
-		      refresh: 60,
+		      refresh: 90,
 		      navBar: false
 		    }
 	  },
@@ -108,17 +108,17 @@ var reportList =
 	  		rid: 'e46b2373-196a-453d-b7bd-8b694beaabb4',
 	  		gid: 'b1717488-1ca0-4f42-8da3-1fc7c1363833',
 	  		name: 'Bin Fill Rate',
-	  		refresh: 60,
+	  		refresh: 120,
 	  		navBar: false
 	  	}
 	  },
 	openreplentasks: {
 	  	openreplentasks:{
-	  		id: 'binfillrate',
+	  		id: 'openpreplentasks',
 	  		rid: '1e4874f3-ac81-4f0f-ab78-e5e1097947cb',
 	  		gid: 'b1717488-1ca0-4f42-8da3-1fc7c1363833',
 	  		name: 'Open Replen Tasks',
-	  		refresh: 60,
+	  		refresh: 120,
 	  		navBar: false
 	  	}
 	  },
@@ -128,7 +128,7 @@ var reportList =
 	  		rid: '49c0af6d-7903-4430-8bf0-c1ab494be0cf',
 	  		gid: 'b1717488-1ca0-4f42-8da3-1fc7c1363833',
 	  		name: 'Not Loaded',
-	  		refresh: 60,
+	  		refresh: 90,
 	  		navBar: false
 	  	},
 	  	plantscorecard:{
@@ -136,7 +136,7 @@ var reportList =
 	  		rid: 'b60ee07d-f257-4aac-b341-5db2f13e67f6',
 	  		gid: 'b1717488-1ca0-4f42-8da3-1fc7c1363833',
 	  		name: 'Plant Scorecard',
-	  		refresh: 60,
+	  		refresh: 90,
 	  		navBar: false
 	  	},
 	  	dispatch:{
@@ -144,7 +144,7 @@ var reportList =
 	  		rid: '697d9583-a0c4-4c64-9262-4d1095726311',
 	  		gid: 'b1717488-1ca0-4f42-8da3-1fc7c1363833',
 	  		name: 'Dispatch',
-	  		refresh: 60,
+	  		refresh: 90,
 	  		navBar: false
 	  	},
 	  	undeliveredboxes:{
@@ -152,7 +152,7 @@ var reportList =
 	  		rid: 'db69c2ec-e3dc-4b18-8c12-814c1bd53f12',
 	  		gid: 'b1717488-1ca0-4f42-8da3-1fc7c1363833',
 	  		name: 'Undelivered Boxes',
-	  		refresh: 60,
+	  		refresh: 90,
 	  		navBar: false
 	  	},
 	  	opscenter:{
@@ -160,7 +160,7 @@ var reportList =
 	  		rid: '23ab3b2b-e94f-4e5b-ab41-b59e174c3b8a',
 	  		gid: 'b1717488-1ca0-4f42-8da3-1fc7c1363833',
 	  		name: 'Ops Center',
-	  		refresh: 60,
+	  		refresh: 90,
 	  		navBar: false
 	  	}
 	  },
@@ -170,7 +170,7 @@ var reportList =
 	  		rid: '708f24e6-a672-4de9-a529-9f025dc1ecdb',
 	  		gid: 'b1717488-1ca0-4f42-8da3-1fc7c1363833',
 	  		name: 'Late Boxes',
-	  		refresh: 60,
+	  		refresh: 90,
 	  		navBar: false
 	  	},
 	  	plantdispatch:{
@@ -178,7 +178,7 @@ var reportList =
 	  		rid: 'c89fa47f-5e99-45b8-a608-d0f15583a28e',
 	  		gid: 'b1717488-1ca0-4f42-8da3-1fc7c1363833',
 	  		name: 'Plant Dispatch',
-	  		refresh: 60,
+	  		refresh: 90,
 	  		navBar: false
 	  	},
 	  	undeliveredboxes:{
@@ -186,7 +186,7 @@ var reportList =
 	  		rid: 'd64c0d1c-c120-4ffb-a554-805fc22cc878',
 	  		gid: 'b1717488-1ca0-4f42-8da3-1fc7c1363833',
 	  		name: 'Undelivered Boxes',
-	  		refresh: 60,
+	  		refresh: 90,
 	  		navBar: false
 	  	},
 	  	opscenter:{
@@ -194,7 +194,7 @@ var reportList =
 	  		rid: '7e0d63cb-a498-4ddb-86a9-cf05d7227c25',
 	  		gid: 'b1717488-1ca0-4f42-8da3-1fc7c1363833',
 	  		name: 'Ops Center',
-	  		refresh: 60,
+	  		refresh: 90,
 	  		navBar: false
 	  	},
 	  	transportationdispatch:{
@@ -202,7 +202,7 @@ var reportList =
 	  		rid: 'd044cceb-5220-40be-b06d-e2d106323ced',
 	  		gid: 'b1717488-1ca0-4f42-8da3-1fc7c1363833',
 	  		name: 'Transportation Dispatch',
-	  		refresh: 60,
+	  		refresh: 90,
 	  		navBar: false
 	  	}
 	  },
@@ -248,7 +248,7 @@ var reportList =
 	  		rid: 'bc184bbb-bf69-490a-8b78-82a88a2f3343',
 	  		gid: 'b1717488-1ca0-4f42-8da3-1fc7c1363833',
 	  		name: 'Post Wave Monitor',
-	  		refresh: 60,
+	  		refresh: 120,
 	  		navBar: false
 	  	}
 	  },
@@ -258,7 +258,7 @@ var reportList =
 	  		rid: 'ffc4834b-9251-46b0-aa5f-1e9530660070',
 	  		gid: 'b1717488-1ca0-4f42-8da3-1fc7c1363833',
 	  		name: 'Start Of Production',
-	  		refresh: 60,
+	  		refresh: 120,
 	  		navBar: false
 	  	}
 	  },
@@ -268,7 +268,7 @@ var reportList =
 	  		rid: '5220edda-128e-44c2-9d6d-6a2012ded56f',
 	  		gid: 'b1717488-1ca0-4f42-8da3-1fc7c1363833',
 	  		name: 'Pending Picks',
-	  		refresh: 60,
+	  		refresh: 120,
 	  		navBar: false
 	  	}
 	  },
@@ -278,7 +278,7 @@ var reportList =
 	  		rid: '7bf95e7c-d60d-4022-8962-a81c6e94fc58',
 	  		gid: 'b1717488-1ca0-4f42-8da3-1fc7c1363833',
 	  		name: 'Pre-wave Monitor',
-	  		refresh: 60,
+	  		refresh: 120,
 	  		navBar: false
 	  	}
 	  },
@@ -288,7 +288,7 @@ var reportList =
 	  		rid: '7defe7aa-1a2a-405a-bff7-2fb8acf34564',
 	  		gid: 'b1717488-1ca0-4f42-8da3-1fc7c1363833',
 	  		name: 'Intraday Operations',
-	  		refresh: 60,
+	  		refresh: 120,
 	  		navBar: false
 	  	}
 	  },
@@ -298,7 +298,7 @@ var reportList =
 	  		rid: 'f0a70cd1-dc41-468c-9866-56a0ac12e445',
 	  		gid: 'b1717488-1ca0-4f42-8da3-1fc7c1363833',
 	  		name: 'Remake Requirements',
-	  		refresh: 60,
+	  		refresh: 120,
 	  		navBar: false
 	  	}
 	  },
@@ -308,7 +308,7 @@ var reportList =
 	  		rid: '76505246-b574-4a28-9f03-60b2911a7023',
 	  		gid: 'b1717488-1ca0-4f42-8da3-1fc7c1363833',
 	  		name: 'Prod Wave Monitor',
-	  		refresh: 60,
+	  		refresh: 120,
 	  		navBar: false
 	  	}
 	  }
